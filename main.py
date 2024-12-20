298de307c860a11ec773695dd113c1948baabf2c
Mohammad Chaar <mhmdchaarrr@gmail.com>
2024-12-21 00:43:55 +0100

    using the adding images to make the logo and title of the game

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ pygame.init
 #setting up the width and height to set it up for the screen or display size
 width = 1400
 height = 800
+
 #uses the width and height variables for the screen size 
 screen = pygame.display.set_mode((width, height))
+
+#loading the images and connecting them to variables
+
+logo = pygame.image.load('logo.png')
+bg = pygame.image.load('background.jpg')
+
+#nameing the the title of the game and adding the logo of the game
+pygame.display.set_caption("Srh Project - Head Soccer")
+pygame.display.set_icon(logo)
 
 #event loop , when variable running is true which tells us the screen will run while it is true 
 running = True
